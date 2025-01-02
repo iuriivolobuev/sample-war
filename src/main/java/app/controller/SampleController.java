3d65ab679ab54c3d4bc2d85a68ec96de3808d259
Iurii Volobuev <iurii.volobuev@gmail.com>
2025-01-02 18:01:15 +0100

    Add sample war for tomcat-11

---
+++ src/main/java/app/controller/SampleController.java
@@ -0,0 +1,12 @@
+package app.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class SampleController {
+  @GetMapping("/sample")
+  public String sample() {
+    return "Sample";
+  }
+}
